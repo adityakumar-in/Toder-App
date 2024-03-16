47deacf44f971013dce3817743704f4656d01910
Aditya Kumar <adityaakumar.in@gmail.com>
2024-03-16 23:05:19 +0530

    Started Designing SignUp Screen, All Implementing Libraries Imported

---
+++ app/src/main/java/com/adityadev/toder/EmailLoginInActivity.kt
@@ -0,0 +1,11 @@
+package com.adityadev.toder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailLoginInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_login_in)
+    }
+}
